faf39bc6d7722c007b06400d0523b9c695245692
CuteDino <rubin4ik5553535@gmail.com>
2021-04-29 18:47:51 +0700

    Oh

--- app/src/main/java/com/example/manul_dice/Manul_Obmanul.kt
+++ app/src/main/java/com/example/manul_dice/Manul_Obmanul.kt
@@ -4,6 +4,7 @@ import android.media.AudioManager
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
@@ -26,6 +27,12 @@ class Manul_Obmanul : AppCompatActivity() {
     private var soundId10 = 0
     private var soundId11 = 0
     private var soundId12 = 0
+    private var soundId13 = 0
+    private var soundId14 = 0
+    private var soundId15 = 0
+    private var soundId16 = 0
+    private var soundId17 = 0
+    private var soundId18 = 0
 
     fun playSound(sound: Int) {
         var soundId = 0
@@ -42,6 +49,13 @@ class Manul_Obmanul : AppCompatActivity() {
             10 -> soundId = soundId10
             11 -> soundId = soundId11
             12 -> soundId = soundId12
+
+            13 -> soundId = soundId13 //raboch
+            14 -> soundId = soundId14//raboch
+            15 -> soundId = soundId15//crash
+            16 -> soundId = soundId16//closedh
+            17 -> soundId = soundId17//openh
+            18 -> soundId = soundId18//bochka
         }
         soundPool?.play(soundId, 1F, 1F, 0, 0, 1F)
     }
@@ -66,14 +80,23 @@ class Manul_Obmanul : AppCompatActivity() {
         soundId11 = soundPool!!.load(baseContext, R.raw.eleven, 1)
         soundId12 = soundPool!!.load(baseContext, R.raw.twelve, 1)
 
+        soundId13 = soundPool!!.load(baseContext, R.raw.rabochiy, 1)
+        soundId14 = soundPool!!.load(baseContext, R.raw.rabochiy2, 1)
+        soundId15 = soundPool!!.load(baseContext, R.raw.crash, 1)
+        soundId16 = soundPool!!.load(baseContext, R.raw.hihatclosed1, 1)
+        soundId17 = soundPool!!.load(baseContext, R.raw.hihatopened1, 1)
+        soundId18 = soundPool!!.load(baseContext, R.raw.bochka, 1)
 
 
-        findViewById<ImageView>(R.id.iv_one).setOnClickListener { MainScope().launch { playSound(1) } }
-        findViewById<ImageView>(R.id.iv_two).setOnClickListener { MainScope().launch { playSound(2) } }
-        findViewById<ImageView>(R.id.iv_three).setOnClickListener { MainScope().launch { playSound(3) } }
-        findViewById<ImageView>(R.id.iv_four).setOnClickListener { MainScope().launch { playSound(4) } }
-        findViewById<ImageView>(R.id.iv_five).setOnClickListener { MainScope().launch { playSound(5) } }
-        findViewById<ImageView>(R.id.iv_six).setOnClickListener { MainScope().launch { playSound(6) } }
+//        findViewById<ImageView>(R.id.iv_one).setOnClickListener { MainScope().launch { playSound(1) } }
+//        findViewById<ImageView>(R.id.iv_two).setOnClickListener { MainScope().launch { playSound(2) } }
+//        findViewById<ImageView>(R.id.iv_three).setOnClickListener { MainScope().launch { playSound(3) } }
+
+
+
+        findViewById<ImageView>(R.id.iv_four).setOnClickListener { MainScope().launch { playSound(18) } }
+        findViewById<ImageView>(R.id.iv_five).setOnClickListener { MainScope().launch { playSound(16) } }
+        findViewById<ImageView>(R.id.iv_six).setOnClickListener { MainScope().launch { playSound(13) } }
 
 
 
@@ -148,5 +171,14 @@ class Manul_Obmanul : AppCompatActivity() {
         }
     }
 
+    fun playSound18(view: View) {
+        soundPool?.play(soundId18, 1F, 1F, 0, 0, 1F)
+    }
+    fun playSound16(view: View) {
+        soundPool?.play(soundId16, 1F, 1F, 0, 0, 1F)
+    }
+    fun playSound13(view: View) {
+        soundPool?.play(soundId13, 1F, 1F, 0, 0, 1F)
+    }
 
 }
